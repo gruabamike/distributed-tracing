19fa724a056fd2b93f27456c1cdb9e434fd95f6a
mgruber <michael_95@gmx.at>
2022-10-05 00:59:34 +0200

    added open telemetry tracing

--- src/Services/Users.API/Program.cs
+++ src/Services/Users.API/Program.cs
@@ -1,10 +1,27 @@
 using DistributedTracingDotNet.Services.Users.API.Data;
 using DistributedTracingDotNet.Services.Users.API.Services;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Reflection;
+
+var serviceName = $"{nameof(DistributedTracingDotNet)}.Users.API";
+var serviceVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.0.0";
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddOpenTelemetryTracing(config => config
+    .AddConsoleExporter()
+    .AddSource(serviceName)
+    .SetResourceBuilder(
+        ResourceBuilder.CreateDefault()
+            .AddService(serviceName, serviceVersion: serviceVersion))
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation()
+    .AddSqlClientInstrumentation()
+);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
