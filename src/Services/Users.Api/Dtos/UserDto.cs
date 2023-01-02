bd30c631e950d96e7e766e8746aa615f88498ac8
gruabamike <76966174+gruabamike@users.noreply.github.com>
2023-01-02 22:00:50 +0100

    Delete src/Services/Users.Api directory

--- src/Services/Users.Api/Dtos/UserDto.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace DistributedTracingDotNet.Services.Users.Api.Dtos;
-
-public record UserDto(Guid Id, string FirstName, string LastName);
--- src/Services/Users.Api/Dtos/UserForCreationDto.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace DistributedTracingDotNet.Services.Users.Api.Dtos;
-
-public record UserForCreationDto(string FirstName, string LastName);
